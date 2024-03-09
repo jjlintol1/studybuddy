77c3825d81398446e2e053ec7317492358fab0d3
JJ Lintol <jjlintol@JJs-MacBook-Pro.local>
2024-03-09 15:16:15 -0500

    home page ui

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,9 +1,90 @@
-import React from 'react'
+import GenerateSetLink from "@/components/home/GenerateSetLink";
+import StudySetCarousel from "@/components/shared/carousel/StudySetCarousel";
+import UserCarousel from "@/components/shared/carousel/UserCarousel";
+
+import React from "react";
+
+const dummySets = [
+  {
+    id: 1,
+    name: "Example Set 1",
+    cards: 10,
+    author: {
+      name: "John Doe",
+      avatar: "/assets/images/avatar.png",
+    },
+  },
+  {
+    id: 2,
+    name: "Example Set 2",
+    cards: 10,
+    author: {
+      name: "John Doe",
+      avatar: "/assets/images/avatar.png",
+    },
+  },
+  {
+    id: 3,
+    name: "Example Set 3",
+    cards: 10,
+    author: {
+      name: "John Doe",
+      avatar: "/assets/images/avatar.png",
+    },
+  },
+  {
+    id: 4,
+    name: "Example Set 4",
+    cards: 10,
+    author: {
+      name: "John Doe",
+      avatar: "/assets/images/avatar.png",
+    },
+  },
+];
+
+const dummyUsers = [
+  {
+    id: 1,
+    avatar: "/assets/images/avatar.png",
+    username: "johndoe21",
+    sets: 10,
+  },
+  {
+    id: 2,
+    avatar: "/assets/images/avatar.png",
+    username: "johndoe21",
+    sets: 10,
+  },
+  {
+    id: 3,
+    avatar: "/assets/images/avatar.png",
+    username: "johndoe21",
+    sets: 10,
+  },
+  {
+    id: 4,
+    avatar: "/assets/images/avatar.png",
+    username: "johndoe21",
+    sets: 10,
+  },
+  {
+    id: 5,
+    avatar: "/assets/images/avatar.png",
+    username: "johndoe21",
+    sets: 10,
+  },
+];
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <>
+      <StudySetCarousel title="Your Sets" sets={dummySets} isTop />
+      <GenerateSetLink />
+      <StudySetCarousel title="Trending Sets" sets={dummySets} isTop={false} />
+      <UserCarousel users={dummyUsers} />
+    </>
   );
-}
+};
 
-export default HomePage
+export default HomePage;
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: "www.gravatar.com",
+                pathname: "/avatar/*",
+            }
+        ]
+    }
+};
 
 export default nextConfig;
