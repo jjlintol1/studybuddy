85b3d511f5c2447c479d51a4ae037362d5c5489f
JJ Lintol <jjlintol@JJs-MacBook-Pro.local>
2024-03-09 12:18:34 -0500

    first commit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <main className="background-light850_dark100 relative">
+      <div className="flex">
+        <section className="flex min-h-screen flex-1 flex-col overflow-y-auto px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
+          <div className="mx-auto w-full max-w-5xl">{children}</div>
+        </section>
+      </div>
+    </main>
+  );
+};
+
+export default RootLayout;
