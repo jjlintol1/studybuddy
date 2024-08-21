801002c938d98d56cb9f220d0162d6beada3cb0e
JJ Lintol <jjlintol@JJs-MacBook-Pro.local>
2024-08-21 07:36:32 -0400

    change middleware again

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,9 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
+// const isProtectedRoute = createRouteMatcher(["/client"]);
+
 export default clerkMiddleware();
 
 export const config = {
-    matcher: [
-      // Skip Next.js internals and all static files, unless found in search params
-      '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-      // Always run for API routes
-      '/(api|trpc)(.*)',
-    ],
-  }
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
