52098146b5e6813b4e9ec0b39fe62b6a7d03e677
JJ Lintol <jjlintol@JJs-MacBook-Pro.local>
2024-08-20 20:02:12 -0400

    remove useRouter

--- components/shared/sidebar/LeftSidebar.tsx
+++ components/shared/sidebar/LeftSidebar.tsx
@@ -4,7 +4,7 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import { usePathname, useRouter } from "next/navigation";
+import { usePathname } from "next/navigation";
 
 import { sidebarLinks } from "@/constants";
 // import { createClient } from "@/lib/supabase/client";
